62df107c122e8978f1a348f37daa32f1adecd231
Elvira <elveechernenko@yandex.ru>
2024-02-17 20:01:33 +0300

    Изменяет адрес сервера

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,7 +15,7 @@ const StatusCodeMapping: Record<number, boolean> = {
 };
 
 const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
-const BACKEND_URL = 'https://grading.design.pages.academy/v1/escape-room/';
+const BACKEND_URL = 'https://grading.design.htmlacademy.pro/v1/escape-room/';
 const REQUEST_TIMEOUT = 5000;
 
 export const createAPI = (): AxiosInstance => {
